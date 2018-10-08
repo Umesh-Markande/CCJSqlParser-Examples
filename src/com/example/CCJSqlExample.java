e1de7df12360057261adf913065374967b5ee891
Umesh Markande <umeshm5555@gmail.com>
2018-10-08 08:39:08 +0530

    Update CCJSqlExample.java

--- src/com/example/CCJSqlExample.java
+++ src/com/example/CCJSqlExample.java
@@ -7,7 +7,6 @@ import java.util.List;
 import org.apache.commons.lang3.StringUtils;
 
 import net.sf.jsqlparser.JSQLParserException;
-import net.sf.jsqlparser.expression.Alias;
 import net.sf.jsqlparser.expression.BinaryExpression;
 import net.sf.jsqlparser.expression.Expression;
 import net.sf.jsqlparser.expression.Parenthesis;
@@ -28,88 +27,91 @@ import net.sf.jsqlparser.statement.select.SelectItem;
 import net.sf.jsqlparser.statement.select.SelectVisitorAdapter;
 import net.sf.jsqlparser.statement.select.SubJoin;
 import net.sf.jsqlparser.statement.select.SubSelect;
+import net.sf.jsqlparser.util.AddAliasesVisitor;
 import net.sf.jsqlparser.util.deparser.ExpressionDeParser;
 
 public class CCJSqlExample {
-	
-	private static int counter = 0;
-	private static String prefix = "A";
-	
+
 	public static void main(String[] args) throws JSQLParserException {
-		String varname1 = ""
-				+ "SELECT hallprefs.studentname as fdsf "
-				+ "    , hh.studentid as dfsd "
-				+ "    , hh.studentdesc "
-				+ "    , hh.hallname,(select te from ta where fsdf = fsdf) "
-				+ "FROM students "
-				+ "INNER JOIN hallprefs "
+		String varname1 = "" + "SELECT hallprefs.studentname as fdsf " + "    , hh.studentid as dfsd "
+				+ "    , hh.studentdesc " + "    , hh.hallname,(select te from ta where fsdf = fsdf) "
+				+ "FROM students " + "INNER JOIN hallprefs "
 				+ "    on ss.studentid = hallprefs.studentid left outer join fdsff on fsdfsfd = fsff "
-				+ "INNER JOIN halls hh "
-				+ "    on hp.hallid = hh.hallid cross join (select tst from tsdfs) where gh=jgjh and tsfds = sdfd or fsdfsf = fdsff and (fdsfsf = fdsf or fdsfsf = fdfsf) ";
-				//+ "    on hp.hallid = hh.hallid cross join dfdf where gh=jgjh ";
-		
+				+ "INNER JOIN halls hh " + "    on hp.hallid = hh.hallid cross join (select tst from tsdfs) where "
+				+ " gh=jgjh and tsfds = sdfd or fsdfsf = fdsff and (fdsfsf = fdsf or fdsfsf = fdfsf)"
+				+ " and lfsdf like '%fdsf%' and (fsdfs between 99 and 778) and fsdfs < 877 ";
+		// + " on hp.hallid = hh.hallid cross join dfdf where gh=jgjh ";
+
 		Statement st = CCJSqlParserUtil.parse(varname1);
 		Select select = (Select) st;
+
+		// set aliase if not provide
+
+		AddAliasesVisitor instance = new AddAliasesVisitor();
+		instance.setPrefix("A");
+		select.getSelectBody().accept(instance);
+
 		PlainSelect ps = (PlainSelect) select.getSelectBody();
-		
-		//set aliase if not provided
-		
-		setAliase(select);
-		
+
+		System.out.println(ps.toString());
+
 		// select column item visitor with sub select
-		
-		List<String> selectColumnAliase = new ArrayList<>(); 
-		
-		for (SelectItem item : ((PlainSelect)select.getSelectBody()).getSelectItems()) {
-			 if (item instanceof SelectExpressionItem) {
-				 Expression expr = ((SelectExpressionItem)item).getExpression();
-				 if (item instanceof SelectExpressionItem) {
-					 Expression subexpr = ((SelectExpressionItem)item).getExpression();
-					 if (expr instanceof SubSelect) {
-						 //((SubSelect)expr).getSelectBody()) // iterat sub select item
-						 System.out.println("sub select");
-					 }
-				 }
-				 if(expr instanceof  Column){
-		                Column col = (Column) expr;
-		                //System.out.println(col.getColumnName());		// only column name without alise  hh.dffs output dffs
-		                Table table = col.getTable();
-		                System.out.println(table.getFullyQualifiedName());	// Table alias of column like hh.test output hh
-		                selectColumnAliase.add("".equals(table.getFullyQualifiedName()) ? col.getColumnName() : table.getFullyQualifiedName());
-		            }
-			 }
-		}
-		
-		// from table visitor
-		
-		FromItem fromItem = ps.getFromItem();
-		
-		String fromAliase = fromItem.getAlias() != null ? fromItem.getAlias().toString() : "";
-		
-		System.out.println(fromAliase);
-		
-		// join sub select item visitor
-		
-		select.getSelectBody().accept(new SelectVisitorAdapter(){
-		    @Override
-		    public void visit(PlainSelect plainSelect) {
-			plainSelect.getFromItem().accept(fromJoinSubSelectVisitor);
-			if (plainSelect.getJoins()!=null){
-			   plainSelect.getJoins().forEach(join -> join.getRightItem().accept(fromJoinSubSelectVisitor));
+
+		List<String> selectColumnAliase = new ArrayList<>();
+
+		for (SelectItem item : ((PlainSelect) select.getSelectBody()).getSelectItems()) {
+			if (item instanceof SelectExpressionItem) {
+				Expression expr = ((SelectExpressionItem) item).getExpression();
+				if (item instanceof SelectExpressionItem) {
+					Expression subexpr = ((SelectExpressionItem) item).getExpression();
+					if (expr instanceof SubSelect) {
+						// ((SubSelect)expr).getSelectBody()) // iterat sub select item
+						System.out.println("sub select");
+					}
+				}
+				if (expr instanceof Column) {
+					Column col = (Column) expr;
+					// System.out.println(col.getColumnName()); // only column name without alise
+					// hh.dffs output dffs
+					Table table = col.getTable();
+					// System.out.println(table.getFullyQualifiedName());
+					selectColumnAliase.add("".equals(table.getFullyQualifiedName()) ? col.getColumnName()
+							: table.getFullyQualifiedName());
+				}
 			}
-		    }
+		}
+
+		// from table visitor
+
+		FromItem fromItem = ps.getFromItem();
+
+		String fromAliase = fromItem.getAlias() != null ? fromItem.getAlias().toString() : "";
+
+		System.out.println(fromAliase);
+
+		// join sub select item visitor
+
+		select.getSelectBody().accept(new SelectVisitorAdapter() {
+			@Override
+			public void visit(PlainSelect plainSelect) {
+				plainSelect.getFromItem().accept(fromJoinSubSelectVisitor);
+				if (plainSelect.getJoins() != null) {
+					plainSelect.getJoins().forEach(join -> join.getRightItem().accept(fromJoinSubSelectVisitor));
+				}
+			}
 		});
-		
-		//join table visitor
-		
+
+		// join table visitor
+
 		List<Integer> removeJoinIndex = new ArrayList<>();
 		int index = 0;
 		for (Iterator<Join> joinsIt = ps.getJoins().iterator(); joinsIt.hasNext();) {
 			Join join = (Join) joinsIt.next();
 			System.out.println(join.getRightItem().getAlias());
-			if(join.getRightItem() != null) {
-				String aliase =  join.getRightItem().getAlias() != null ? join.getRightItem().getAlias().getName() : join.getRightItem().toString();
-				if(!selectColumnAliase.contains(aliase) && !aliase.toLowerCase().contains("select")){
+			if (join.getRightItem() != null) {
+				String aliase = join.getRightItem().getAlias() != null ? join.getRightItem().getAlias().getName()
+						: join.getRightItem().toString();
+				if (!selectColumnAliase.contains(aliase) && !aliase.toLowerCase().contains("select")) {
 					removeJoinIndex.add(index);
 				}
 			}
@@ -118,128 +120,98 @@ public class CCJSqlExample {
 		for (Integer integer : removeJoinIndex) {
 			ps.getJoins().remove(integer.intValue());
 		}
-		
+
 		// where condition visitor
-		ps.setWhere(new StringValue("{{"+parseWhere(ps.getWhere().toString())+"}}") );
-		
+		ps.setWhere(new StringValue("{{" + parseWhere(ps.getWhere().toString()) + "}}"));
+
 		String query = ps.toString();
 		query = query.replace("'{{", "");
 		query = query.replace("}}'", "");
-       		System.out.println(query);
-	
-	}	
+		System.out.println(query);
+
+	}
+
 	private static void processJoinItem(FromItem fromItem) {
-        	System.out.println("fromItem=" + fromItem);
-    	}
-	
+		System.out.println("fromItem=" + fromItem);
+	}
+
 	private final static FromItemVisitorAdapter fromJoinSubSelectVisitor = new FromItemVisitorAdapter() {
 		@Override
 		public void visit(SubSelect subSelect) {
-		    System.out.println("subselect=" + subSelect);
-		    PlainSelect ps = (PlainSelect) subSelect.getSelectBody();
-		    /*String whereCondition = String.valueOf(ps.getWhere());
-		    if(ps.getWhere() != null) {
-			// iterate where condition
-		    }*/
+			System.out.println("subselect=" + subSelect);
+			PlainSelect ps = (PlainSelect) subSelect.getSelectBody();
+			/*
+			 * String whereCondition = String.valueOf(ps.getWhere()); if(ps.getWhere() !=
+			 * null) { // iterate where condition }
+			 */
 
 		}
 
 		@Override
 		public void visit(Table table) {
-		    System.out.println("table=" + table);
+			System.out.println("table=" + table);
 		}
 
-		public void visit(SubJoin  subjoin) {
+		public void visit(SubJoin subjoin) {
 			System.out.println(subjoin.toString());
 		}
-    	} ;
+	};
 
-    	public static String parseWhere(String where) throws JSQLParserException {
+	public static String parseWhere(String where) throws JSQLParserException {
 		Expression expr = CCJSqlParserUtil.parseCondExpression(where);
 		StringBuilder b = new StringBuilder();
 		expr.accept(new ExpressionDeParser(null, b) {
-		    int depth = 0;
-		    @Override
-		    public void visit(Parenthesis parenthesis) {
-			if (parenthesis.isNot()) {
-			    getBuffer().append("NOT");
-			}
-			depth++;
-			parenthesis.getExpression().accept(this);
-			depth--;
-		    } 
-		   @Override
-		    public void visit(OrExpression orExpression) {
-			visitBinaryExpr(orExpression, "OR");
-		    }
-		    @Override
-		    public void visit(AndExpression andExpression) {
-			visitBinaryExpr(andExpression, "AND");
-		    }
+			int depth = 0;
 
-		    private void visitBinaryExpr(BinaryExpression expr, String operator) {
-			if (expr.isNot()) {
-			    getBuffer().append("NOT");
+			@Override
+			public void visit(Parenthesis parenthesis) {
+				if (parenthesis.isNot()) {
+					getBuffer().append("NOT");
+				}
+				depth++;
+				parenthesis.getExpression().accept(this);
+				depth--;
 			}
-			if (!(expr.getLeftExpression() instanceof OrExpression)
-				&& !(expr.getLeftExpression() instanceof AndExpression)
-				&& !(expr.getLeftExpression() instanceof Parenthesis)) {
-			     String string = expr.getLeftExpression().toString();
-			     System.out.println(string);
+
+			@Override
+			public void visit(OrExpression orExpression) {
+				visitBinaryExpr(orExpression, "OR");
 			}
-			expr.getLeftExpression().accept(this);
-			if (!(expr.getRightExpression() instanceof OrExpression)
-				&& !(expr.getRightExpression() instanceof AndExpression)
-				&& !(expr.getRightExpression() instanceof Parenthesis)) {
-			    String string = expr.getRightExpression().toString();
-			    System.out.println(string);
+
+			@Override
+			public void visit(AndExpression andExpression) {
+				visitBinaryExpr(andExpression, "AND");
 			}
-			expr.getRightExpression().accept(this);
-		    }
+
+			private void visitBinaryExpr(BinaryExpression expr, String operator) {
+				if (expr.isNot()) {
+					getBuffer().append("NOT");
+				}
+				if (!(expr.getLeftExpression() instanceof OrExpression)
+						&& !(expr.getLeftExpression() instanceof AndExpression)
+						&& !(expr.getLeftExpression() instanceof Parenthesis)) {
+					String string = expr.getLeftExpression().toString();
+					System.out.println(string);
+				}
+				expr.getLeftExpression().accept(this);
+				if (!(expr.getRightExpression() instanceof OrExpression)
+						&& !(expr.getRightExpression() instanceof AndExpression)
+						&& !(expr.getRightExpression() instanceof Parenthesis)) {
+					String string = expr.getRightExpression().toString();
+					System.out.println(string);
+				}
+				expr.getRightExpression().accept(this);
+			}
 		});
-	       return expr.toString();
-    	}
-	
-	public static void  setAliase(Select select) {
-		boolean firstRun = true;
-		List<String> aliases = new ArrayList<>();
-		for (SelectItem item : ((PlainSelect)select.getSelectBody()).getSelectItems()) {
-			 if (item instanceof SelectExpressionItem) {
-				 Expression expr = ((SelectExpressionItem)item).getExpression();
-				 //System.out.println(((SelectExpressionItem)item).getAlias());
-				 SelectExpressionItem selectExpressionItem = ((SelectExpressionItem)item); 
-				 if (firstRun) {
-						if (selectExpressionItem.getAlias() != null) {
-							aliases.add(selectExpressionItem.getAlias().getName().toUpperCase());
-						}
-						firstRun = false;
-					} else {
-						if (selectExpressionItem.getAlias() == null) {
+		return expr.toString();
+	}
 
-								String alias = getNextAlias().toUpperCase();
-								while (true) {
-								if (!aliases.contains(alias)) {
-									aliases.add(alias);
-									selectExpressionItem.setAlias(new Alias(alias));
-									break;
-								}
-								}
-						}
-					}
-			 }
-		}
-	}
-	public static String getNextAlias() {
-		counter++;
-		return prefix + counter;
-	}
 	protected static String getEntityAlias(String entityName, PlainSelect query) {
-		/*for (Object o : query.getSelectItems()) {
-			SelectItem st = (SelectItem) o;
-			if (hasEntityAlias(entityName, st)) {
-				return join.getRightItem().getAlias().toString();
-			}
-		}*/
+		/*
+		 * for (Object o : query.getSelectItems()) { SelectItem st = (SelectItem) o; if
+		 * (hasEntityAlias(entityName, st)) { return
+		 * join.getRightItem().getAlias().toString(); } }
+		 */
 		FromItem fromItem = query.getFromItem();
 		if (hasEntityAlias(entityName, fromItem)) {
 			return fromItem.getAlias().toString();
@@ -254,15 +226,16 @@ public class CCJSqlExample {
 		}
 		return null;
 	}
-	
+
 	private static boolean hasEntityAlias(String entityName, FromItem fromItem) {
 		return fromItem instanceof net.sf.jsqlparser.schema.Table
 				&& ((net.sf.jsqlparser.schema.Table) fromItem).getName().equals(entityName)
 				&& !StringUtils.isBlank(fromItem.getAlias().toString());
 	}
-	/*private static boolean hasEntityAlias(String entityName, SelectItem selectitem) {
-		return selectitem instanceof net.sf.jsqlparser.schema.Column
-				&& ((net.sf.jsqlparser.schema.Column) selectitem).getName().equals(entityName)
-				&& !StringUtils.isBlank(selectitem. .toString());
-	}*/
+	/*
+	 * private static boolean hasEntityAlias(String entityName, SelectItem
+	 * selectitem) { return selectitem instanceof net.sf.jsqlparser.schema.Column &&
+	 * ((net.sf.jsqlparser.schema.Column) selectitem).getName().equals(entityName)
+	 * && !StringUtils.isBlank(selectitem. .toString()); }
+	 */
 }
